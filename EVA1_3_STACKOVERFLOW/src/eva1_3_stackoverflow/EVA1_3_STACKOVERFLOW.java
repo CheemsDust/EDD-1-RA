5f93bd65ffdd84f6fca314359ba53399f7a66c89
Luis Calzadillas <L24550259@chihuahua2.tecnm.mx>
2026-09-03 16:49:40 -0600

    Add files via upload

---
+++ EVA1_3_STACKOVERFLOW/src/eva1_3_stackoverflow/EVA1_3_STACKOVERFLOW.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_stackoverflow;
+
+/**
+ *
+ * @author calza
+ */
+public class EVA1_3_STACKOVERFLOW {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      A();
+      
+   }
+   public static void A(){
+      B();
+   }
+   public static void B(){
+      int i = 5;
+      double d = 5.5;
+      A();
+   }
+   
+}
